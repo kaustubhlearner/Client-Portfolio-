95b518c78bbd7916135c8850187801188250d522
Kaustubh Kumar <164003232+kaustubhlearner@users.noreply.github.com>
2026-09-08 15:57:41 +0530

    Fix navigation, paths and portfolio interactions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import MainSection from "@/components/MainSection";
-import ContactForm from "@/components/ContactForm";
-import ProjectsSection from "@/components/Projectsection";
+import MainSection from '@/components/MainSection';
+import ContactForm from '@/components/ContactForm';
+import ProjectsSection from '@/components/Projectsection';
+
 export default function Home() {
   return (
     <>
